1a6d8c83640c9c258dafaa80a7c960212f2d6dca
Josiah Matlack <josiah.matlack@gmail.com>
2021-12-01 19:59:10 -0800

    Moved 2020 solutions into their own directory, and started a node/typescript project for 2021.

---
+++ 2021/common/utilities.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from 'fs';
+
+const input_file_name = "input.txt";
+var day = "";
+
+export function readInputFile(file_path: string) {
+    try {
+        const source_path = file_path.replace("build/", "");
+        const source_path_parts = source_path.split("/");
+        day = source_path_parts[source_path_parts.length - 1].replace("day", "");
+        const data = readFileSync(`${source_path}/${input_file_name}`, "ascii");
+        const lines = data.split("\n")
+        if (!lines || !lines.length) {
+            throw new Error("Invalid input - check input file!");
+        }
+
+        return lines;
+    } catch (err) {
+        console.error(err);
+    }
+
+    return [];
+}
+
+export function printAnswer(part: number, answer: string) {
+    console.log(`The answer to day ${day}, part ${part} is: ${answer}.`);
+}
---
+++ 2021/day1/solution.ts
@@ -0,0 +1,152 @@
+/*
+--- Day 1: Sonar Sweep ---
+As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+
+For example, suppose you had the following report:
+
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+
+This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
+
+The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+
+To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+
+199 (N/A - no previous measurement)
+200 (INCREASED)
+208 (INCREASED)
+210 (INCREASED)
+200 (decreased)
+207 (INCREASED)
+240 (INCREASED)
+269 (INCREASED)
+260 (decreased)
+263 (INCREASED)
+
+In this example, there are 7 measurements that are larger than the previous measurement.
+
+How many measurements are larger than the previous measurement?
+
+Your puzzle answer was 1502.
+
+--- Part Two ---
+
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
+
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+Your puzzle answer was 1538.
+*/
+
+import { readInputFile, printAnswer } from '../common/utilities'
+
+const inputLines = readInputFile(__dirname);
+
+function countLargerMeasurements() {
+    let previousDepth = -1;
+    let largerMeasurementsCount = -1;
+    for (let i = 0; i < inputLines.length; i++) {
+        if (inputLines[i] == "") {
+            break;
+        }
+
+        const depth = parseInt(inputLines[i]);
+        if (isNaN(depth)) {
+            throw new Error("Invalid value in input - check input file!");
+        }
+        else if (depth > previousDepth) {
+            largerMeasurementsCount += 1;
+        }
+
+        previousDepth = depth;
+    }
+
+    return largerMeasurementsCount;
+}
+
+function countLargerSums() {
+    let depths = [0, 0, 0];
+    for (let i = 0; i < 3; i++) {
+        if (!inputLines[i]) {
+            return 0;
+        }
+
+        const depth = parseInt(inputLines[i]);
+        if (isNaN(depth)) {
+            throw new Error("Invalid value in input - check input file!");
+        }
+
+        depths[i] = depth;
+    }
+
+    let depth1 = depths[0];
+    let depth2 = depths[1];
+    let depth3 = depths[2];
+    let previousSum = (depth1 + depth2 + depth3);
+    let largerSums = 0;
+    for (let i = 3; i < inputLines.length; i++) {
+        if (!inputLines[i]) {
+            break;
+        }
+
+        const depth = parseInt(inputLines[i]);
+        if (isNaN(depth)) {
+            throw new Error("Invalid value in input - check input file!");
+        }
+
+        depth1 = depth2;
+        depth2 = depth3;
+        depth3 = depth;
+        let currentSum = (depth1 + depth2 + depth3);
+        if (currentSum > previousSum) {
+            largerSums += 1;
+        }
+        previousSum = currentSum;
+    }
+
+    return largerSums;
+}
+
+const partOneAnswer = countLargerMeasurements();
+const partTwoAnswer = countLargerSums();
+printAnswer(1, partOneAnswer.toString());
+printAnswer(2, partTwoAnswer.toString());
