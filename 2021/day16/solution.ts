e29c68c12e5aa9bb72a86973c5d4f6fcc5d13b0c
Josiah Matlack <josiah.matlack@gmail.com>
2021-12-11 14:25:54 -0800

    Adding dummy files for days 8-24

---
+++ 2021/day16/solution.ts
@@ -0,0 +1,13 @@
+/*
+
+*/
+
+import { printAnswer, readInputFile } from '../common/utilities';
+
+const inputLines = readInputFile(__dirname);
+
+// const partOneAnswer =
+// printAnswer(1, partOneAnswer.toString());
+
+// const partTwoAnswer =
+// printAnswer(2, partTwoAnswer.toString());
